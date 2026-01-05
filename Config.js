13ce59941e4016119f3757e91336435759ced1b9
Ali Tech X <alitechxbot@gmail.com>
2026-01-05 15:12:39 +0500

    Add configuration file for bot settings

---
+++ Config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  owner: ['923XXXXXXXXX'],
+  ownerName: 'Ali Tech X',
+  botName: 'DRAGON-MD',
+  prefix: '.',
+  mode: 'public',
+  sessionName: 'dragon-md',
+  version: '1.0.0'
+}
