92155e5d5f5bda4d682fe7df96929d8dc4a3e14e
Ali Tech X <alitechxbot@gmail.com>
2026-01-09 16:00:49 +0500

    Delete Convert.jsi

--- Convert.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  command: ['sticker','sticker2','emojimix','tomp3'],
-  run: async ({ sock, msg }) => {
-    if(!msg.quoted) return sock.sendMessage(msg.key.remoteJid,{text:'❌ Reply to media'}, {quoted:msg})
-    const media = await sock.downloadMediaMessage(msg.quoted)
-    // Convert logic here (sticker/audio)
-    await sock.sendMessage(msg.key.remoteJid,{sticker: media},{quoted:msg})
-  }
-}
