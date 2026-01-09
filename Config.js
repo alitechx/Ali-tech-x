39503f9d62cee38a6201bb3785a413e810668c7b
Ali Tech X <alitechxbot@gmail.com>
2026-01-09 16:00:09 +0500

    Delete Config.js

--- Config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  owner: ['923XXXXXXXXX'],
-  ownerName: 'Ali Tech X',
-  botName: 'DRAGON-MD',
-  prefix: '.',
-  mode: 'public',
-  sessionName: 'dragon-md',
-  version: '1.0.0'
-}
