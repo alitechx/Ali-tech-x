408ce9263515a35f915e73a720caa25d6f568993
Ali Tech X <alitechxbot@gmail.com>
2026-01-13 22:23:07 +0500

    Delete Shapar.js

--- Shapar.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  command: ['shapar'],
-  run: async ({ sock, msg, args }) => {
-    const phrases = ['Hello!', 'How are you?', 'Nice to meet you!', '🔥 DRAGON-MD']
-    const text = phrases[Math.floor(Math.random()*phrases.length)]
-    await sock.sendMessage(msg.key.remoteJid,{text},{quoted:msg})
-  }
-}
