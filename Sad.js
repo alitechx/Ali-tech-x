9aee643721e661341ce456149e96e19756d79497
Ali Tech X <alitechxbot@gmail.com>
2026-01-13 22:22:20 +0500

    Delete Sad.js

--- Sad.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  command: ['sad'],
-  run: async ({ sock, msg }) => {
-    const reactions = ['😢','😭','😔','😞','💔']
-    const text = reactions[Math.floor(Math.random()*reactions.length)]
-    await sock.sendMessage(msg.key.remoteJid,{text},{quoted:msg})
-  }
-}
