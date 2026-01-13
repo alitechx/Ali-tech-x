0beda8ec5b1b3e686cce3895ad1475e6861b44a3
Ali Tech X <alitechxbot@gmail.com>
2026-01-13 21:21:37 +0500

    Delete Joke.js

--- Joke.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
-module.exports = {
-  command: ['joke','funny'],
-  run: async ({ sock, msg }) => {
-    const res = await axios.get('https://api.example.com/joke',{timeout:10000})
-    await sock.sendMessage(msg.key.remoteJid,{text:res.data.joke},{quoted:msg})
-  }
-}
