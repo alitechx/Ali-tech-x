50f806bd12a9c7d5da32c76f7aa4dd59a6e86218
Ali Tech X <alitechxbot@gmail.com>
2026-01-13 21:18:21 +0500

    Delete Happy.js

--- Happy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  command: ['happy'],
-  run: async ({ sock, msg }) => {
-    const reactions = ['😀','😁','😆','😎','🥳']
-    const text = reactions[Math.floor(Math.random()*reactions.length)]
-    await sock.sendMessage(msg.key.remoteJid,{text},{quoted:msg})
-  }
-}
