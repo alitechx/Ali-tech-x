735199ed71354a8823b413ddbac0e51b45ad39cc
Ali Tech X <alitechxbot@gmail.com>
2026-01-13 21:16:59 +0500

    Delete Grouplink.js

--- Grouplink.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  command: ['grouplink','link'],
-  run: async ({ sock, msg }) => {
-    const link = await sock.groupInviteCode(msg.key.remoteJid)
-    await sock.sendMessage(msg.key.remoteJid,{text:`🌐 Group Link: https://chat.whatsapp.com/${link}`},{quoted:msg})
-  }
-}
