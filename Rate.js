239cf44d9cc5404fd760c2e7d10eeb763f7ec67b
Ali Tech X <alitechxbot@gmail.com>
2026-01-13 21:26:44 +0500

    Delete Rate.js

--- Rate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  command: ['rate'],
-  run: async ({ sock, msg, args }) => {
-    if(!args[0]) return sock.sendMessage(msg.key.remoteJid,{text:'❌ Tag a user'}, {quoted:msg})
-    const random = Math.floor(Math.random()*101)
-    await sock.sendMessage(msg.key.remoteJid,{text:`💯 I rate ${args[0]} : ${random}/100`},{quoted:msg})
-  }
-}
